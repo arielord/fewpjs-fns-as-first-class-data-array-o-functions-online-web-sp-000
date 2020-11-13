9236d1f7d19bdf5ba72fb7b28be65881d25c61ac
Ariel <arielord@buffalo.edu>
2020-11-13 18:05:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function wakeDog(dogName, dogBreed){
-  let s = `Wake ${dogName} the ${dogBreed}`
+  let s = `Wake ${dogName} the ${dogBreed}`;
   console.log(s);
 }
 function leashDog(dogName, dogBreed){
-  let s = `Leash ${dogName} the ${dogBreed}`
+  let s = `Leash ${dogName} the ${dogBreed}`;
   console.log(s);
 }
 function walkToPark(dogName, dogBreed){
-  let s = `Walk to the park with ${dogName} the ${dogBreed}`)
-  console.log(;
+  let s = `Walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(s);
 }
 function throwFrisbee(dogName, dogBreed){
-  let s = `Throw the frisbee for ${dogName} the ${dogBreed}`)
-  console.log(;
+  let s = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(s);
 }
 function walkHome(dogName, dogBreed){
   let s = `Walk home with ${dogName} the ${dogBreed}`;
